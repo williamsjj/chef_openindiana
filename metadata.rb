e44fda95b82aa5c7e516ce117dce4782a7211fd3
Jason J. W. Williams <williamsjj@digitar.com>
2011-07-26 16:02:59 -0600

    Changed README.

--- metadata.rb
+++ metadata.rb
@@ -2,5 +2,5 @@ maintainer        "DigiTar"
 maintainer_email  "info@digitar.com"
 license           "BSD"
 description      "Installs/Configures OpenIndiana packaging."
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+long_description "Installs/Configures OpenIndiana packaging."
 version          "0.5.0"
